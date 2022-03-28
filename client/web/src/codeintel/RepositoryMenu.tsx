992a93c4056dc0d983813fff3f8df562b8079589
Eric Fritz <eric@eric-fritz.com>
2022-03-28 12:46:20 -0500

    Apply basic styles.

--- client/web/src/codeintel/RepositoryMenu.tsx
+++ client/web/src/codeintel/RepositoryMenu.tsx
@@ -3,21 +3,62 @@ import React from 'react'
 import classNames from 'classnames'
 import BrainIcon from 'mdi-react/BrainIcon'
 
-import { Icon, Menu, MenuButton, MenuDivider, MenuHeader, MenuList } from '@sourcegraph/wildcard'
+import { Badge, Icon, Link, Menu, MenuButton, MenuDivider, MenuHeader, MenuList, Position } from '@sourcegraph/wildcard'
 
-export const RepositoryMenu: React.FunctionComponent<{}> = () => (
-    <Menu className="btn-icon">
-        <>
-            <MenuButton className={classNames('text-decoration-none test-user-nav-item-toggle')}>
-                <Icon as={BrainIcon} />
-            </MenuButton>
-            <MenuList>
-                <MenuHeader>Code intelligence</MenuHeader>
-                <MenuDivider />
-                <div className="px-2 py-1">
-                    <div className="d-flex align-items-center">Some stuff here?</div>
-                </div>
-            </MenuList>
-        </>
-    </Menu>
-)
+import styles from './RepositoryMenu.module.scss'
+
+export interface RepositoryMenuProps {
+    repoName: string
+    revision: string
+    filePath: string
+    actionType: 'nav' | 'dropdown'
+}
+
+export const RepositoryMenu: React.FunctionComponent<RepositoryMenuProps> = ({
+    repoName,
+    revision,
+    filePath,
+    actionType,
+}) =>
+    actionType == 'dropdown' ? (
+        <>CRAP</>
+    ) : (
+        <Menu className="btn-icon">
+            <>
+                <MenuButton className={classNames('text-decoration-none test-user-nav-item-toggle')}>
+                    <Icon as={BrainIcon} />
+                </MenuButton>
+                <MenuList position={Position.bottomEnd} className={styles.dropdownMenu}>
+                    <MenuHeader>Code intelligence</MenuHeader>
+                    <MenuDivider />
+                    <div className="px-2 py-1">
+                        <div className="d-flex align-items-center">
+                            <div className="p-2 text-uppercase">
+                                {/* <Badge>Unavailable</Badge>
+                            <Badge variant="primary">Available</Badge> */}
+                                <Badge variant="success">Enabled</Badge>
+                            </div>
+                            <div className="p-2">
+                                <span>Precise code intelligence</span>
+                                <br />
+
+                                <span>
+                                    `{repoName}@{revision}:{filePath}`
+                                </span>
+                                <br />
+
+                                <span className="text-muted">Last updated: 02/02/2022</span>
+                                <br />
+
+                                {/* <span className="text-muted">80% Java supported</span><br /> */}
+                                {/* <span className="text-muted">Index is failing</span><br /> */}
+                                {/* <Link to="sdf">I want precise support!</Link><br /> */}
+
+                                <Link to="sdf">Enable precise code intelligence</Link>
+                            </div>
+                        </div>
+                    </div>
+                </MenuList>
+            </>
+        </Menu>
+    )
