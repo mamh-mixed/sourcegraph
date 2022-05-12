5d6d7e133eacc894f54167be9ac3e2cec2e902d0
Eric Fritz <eric@eric-fritz.com>
2022-05-11 19:45:28 -0500

    WIP.

---
+++ migrations/codeintel/1652315984/up.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS lsif_data_apidocs_num_dumps CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_apidocs_num_dumps_indexed CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_apidocs_num_pages CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_apidocs_num_search_results_private CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_apidocs_num_search_results_public CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_current_private CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_current_public CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_lang_names_private CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_lang_names_public CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_private CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_public CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_repo_names_private CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_repo_names_public CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_tags_private CASCADE;
+
+DROP TABLE IF EXISTS lsif_data_docs_search_tags_public CASCADE;
+
+-- TODO - any other columns?
