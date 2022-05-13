9cb22814251dec82a2bc46a44b3d6d933a6a2b63
Eric Fritz <eric@eric-fritz.com>
2022-05-13 17:20:12 -0500

    Canonicalize order.

--- internal/database/migration/schemas/formatter_json.go
+++ internal/database/migration/schemas/formatter_json.go
@@ -2,6 +2,7 @@ package schemas
 
 import (
 	"encoding/json"
+	"sort"
 )
 
 type jsonFormatter struct{}
@@ -11,6 +12,39 @@ func NewJSONFormatter() SchemaFormatter {
 }
 
 func (f jsonFormatter) Format(schemaDescription SchemaDescription) string {
+	for i := range schemaDescription.Tables {
+		sortColumnsByName(schemaDescription.Tables[i].Columns)
+		sortIndexes(schemaDescription.Tables[i].Indexes)
+		sortConstraints(schemaDescription.Tables[i].Constraints)
+		sortTriggers(schemaDescription.Tables[i].Triggers)
+	}
+
+	sortEnums(schemaDescription.Enums)
+	sortFunctions(schemaDescription.Functions)
+	sortSequences(schemaDescription.Sequences)
+	sortTables(schemaDescription.Tables)
+	sortViews(schemaDescription.Views)
+
 	serialized, _ := json.MarshalIndent(schemaDescription, "", "    ")
 	return string(serialized)
+}
+
+func sortConstraints(constraint []ConstraintDescription) {
+	sort.Slice(constraint, func(i, j int) bool { return constraint[i].Name < constraint[j].Name })
+}
+
+func sortTriggers(triggers []TriggerDescription) {
+	sort.Slice(triggers, func(i, j int) bool { return triggers[i].Name < triggers[j].Name })
+}
+
+func sortEnums(enums []EnumDescription) {
+	sort.Slice(enums, func(i, j int) bool { return enums[i].Name < enums[j].Name })
+}
+
+func sortFunctions(functions []FunctionDescription) {
+	sort.Slice(functions, func(i, j int) bool { return functions[i].Name < functions[j].Name })
+}
+
+func sortSequences(sequences []SequenceDescription) {
+	sort.Slice(sequences, func(i, j int) bool { return sequences[i].Name < sequences[j].Name })
 }
