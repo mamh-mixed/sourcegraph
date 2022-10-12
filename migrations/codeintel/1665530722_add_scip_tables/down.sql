31a234cbb23cb1e57bc778442275e42534846ad6
Eric Fritz <eric@eric-fritz.com>
2022-10-12 14:30:29 -0500

    Fix down.

--- migrations/codeintel/1665530722_add_scip_tables/down.sql
+++ migrations/codeintel/1665530722_add_scip_tables/down.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS codeintel_scip_symbols;
 DROP TABLE IF EXISTS codeintel_scip_index_documents;
 DROP TABLE IF EXISTS codeintel_scip_documents;
 DROP TABLE IF EXISTS codeintel_scip_symbols_schema_versions;
-DROP TABLE IF EXISTS codeintel_scip_documents_schema_versions;
+DROP TABLE IF EXISTS codeintel_scip_index_documents_schema_versions;
 DROP FUNCTION IF EXISTS update_codeintel_scip_symbols_schema_versions_insert();
-DROP FUNCTION IF EXISTS update_codeintel_scip_documents_schema_versions_insert();
+DROP FUNCTION IF EXISTS update_codeintel_scip_index_documents_schema_versions_insert();
